7bb34ac05b7edc1a5450f58b20838ca5d778385a
Diego Cerna <diegowz21@gmail.com>
2023-10-26 13:51:41 -0500

    project init

---
+++ src/test/java/com/cibertec/assistanceapi/AssistanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.assistanceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistanceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

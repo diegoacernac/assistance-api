a71104d74ed5aa63c0de859f78bb87e29e1df2ea
Ariel-Buleje <Ariel-Buleje@github.com>
2023-10-31 00:19:29 -0500

    Tipo(entity)

---
+++ src/main/java/com/cibertec/assistanceapi/entity/Tipo.java
@@ -0,0 +1,20 @@
+package com.cibertec.assistanceapi.entity;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_tipo")
+public class Tipo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_tipo;
+    private String descripcion;
+    private int estado;
+
+}

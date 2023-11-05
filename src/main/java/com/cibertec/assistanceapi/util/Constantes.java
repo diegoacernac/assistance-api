6a2917fd94c8b14e3b7239cae1f86146bbf378f2
Ariel-Buleje <Ariel-Buleje@github.com>
2023-11-05 15:06:54 -0500

    area

--- src/main/java/com/cibertec/assistanceapi/util/Constantes.java
+++
@@ -1,8 +0,0 @@
-package com.cibertec.assistanceapi.util;
-
-public class Constantes {
-
-	public static final String TYPE_EXCEL = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-	public static final String TYPE_ZIP = "application/zip";
-	public static final String MENSAJE_REG_ERROR = "No se registro, consulte con el administrador.";
-}

cd00c9d0f005d7b608d78b473b3453093c87d759
Ariel-Buleje <Ariel-Buleje@github.com>
2023-11-06 22:26:57 -0500

    tipo

---
+++ src/main/java/com/cibertec/assistanceapi/repositories/TipoRepository.java
@@ -0,0 +1,8 @@
+package com.cibertec.assistanceapi.repositories;
+
+import com.cibertec.assistanceapi.entities.Tipo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoRepository extends JpaRepository<Tipo, Integer>{
+
+}
---
+++ src/main/java/com/cibertec/assistanceapi/services/AreaServiceImpl.java
@@ -0,0 +1,22 @@
+package com.cibertec.assistanceapi.services;
+
+import com.cibertec.assistanceapi.entities.Area;
+import com.cibertec.assistanceapi.repositories.AreaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class AreaServiceImpl implements AreaService{
+
+	@Autowired
+	private AreaRepository Repository;
+
+	@Override
+	public List<Area> listaArea() {
+		return Repository.findAll();
+
+	}
+}
